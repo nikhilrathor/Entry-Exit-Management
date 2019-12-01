d17d03f8b4a32d0fa90504373de722f01c68b977
nikhilrathor <nikhilrathor01@gmail.com>
2019-12-01 17:47:03 +0530

    Visitor details sent to database

--- public/VISITOR.js
+++ public/VISITOR.js
@@ -1,12 +1,8 @@
 $(function () {
-
-    let name = $('#name')
-    let phoneno = $('#phoneno')
-    let email = $('#email')
     let hostList = $('#hostdetails')
-    let hostname = document.getElementById("hostdetails");
+    let checkouttime
     let checkin = $('#checkin')
-    let checkout =$('#checkout')
+    let checkout = $('#checkout')
 
     fetchHosts(function (hosts) {
         hostList.empty()
@@ -15,29 +11,95 @@ $(function () {
         }
     })
 
-    checkin.click(function(){
-        addvisitor(
-            name.val(),
-            phoneno.val(),
-            email.val(),
-            hostname.value,
-            function (addedVisitor) {
-                window.alert("Added " + addedVisitor.name + " to Database")
-            }
-        )
+    checkin.click(function () {
+        let name = $('#name').val()
+        let phoneno = $('#phoneno').val()
+        let email = $('#email').val()
+        let hostphoneno = document.getElementById("hostdetails").value
+        let checkintime = new Date().toLocaleTimeString()
+
+        if (name.length < 1) {
+            window.alert("Name cannot be empty")
+        }
+        else if (phoneno.length < 1) {
+            window.alert("Phone Number cannot be empty")
+        }
+        else if (!(phoneno.match(/^\d{10}$/))) {
+            window.alert("Invalid Phone Number")
+        }
+        else if (email.length < 1) {
+            window.alert("Email cannot be empty")
+        }
+        else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+            window.alert("Invalid Email")
+        }
+        else {
+            addvisitor(
+                name,
+                phoneno,
+                email,
+                hostphoneno,
+                checkintime,
+                function (addedVisitor) {
+                    if (typeof addedVisitor.name !== "undefined")
+                        window.alert("Added " + addedVisitor.name + " to Database")
+                    else
+                        window.alert("Phone Number already registered to other visitor")
+                }
+            )
+        }
     })
 
-    checkout.click(function(){
-        removevisitor(
-            name.val(),
-            phoneno.val(),
-            email.val(),
-            hostname.value,
-            function (removedVisitor) {
-            window.alert("Removed from Database")
-            }
-        )
+    checkout.click(function () {
+        let name = $('#name').val()
+        let phoneno = $('#phoneno').val()
+        let email = $('#email').val()
+        let hostphoneno = document.getElementById("hostdetails").value
+
+        if (name.length < 1) {
+            window.alert("Name cannot be empty")
+        }
+        else if (phoneno.length < 1) {
+            window.alert("Phone Number cannot be empty")
+        }
+        else if (!(phoneno.match(/^\d{10}$/))) {
+            window.alert("Invalid Phone Number")
+        }
+        else if (email.length < 1) {
+            window.alert("Email cannot be empty")
+        }
+        else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+            window.alert("Invalid Email")
+        }
+        else {
+            validate(
+                name,
+                phoneno,
+                email,
+                hostphoneno,
+                function (valid) {
+                    if(valid.toString() == "Hello")
+                    {
+                        removevisitor(
+                            name,
+                            phoneno,
+                            email,
+                            hostphoneno,
+                            function (removedVisitor) {
+                                if(removedVisitor === "1")
+                                    window.alert("Removed from Database")
+                            }
+                        )
+                    }
+                    else
+                    {
+                        window.alert("Visitor Entry does'nt exist")
+                    }
+                }
+            )
+            
+        }
     })
-    
+
 
 })
