53791b908fc92ef594aa49161e99ab8e225e6612
nikhilrathor <nikhilrathor01@gmail.com>
2019-12-01 17:46:07 +0530

    host entries sent to database

--- public/HOST.js
+++ public/HOST.js
@@ -1,17 +1,39 @@
-$(function (){
-    let name = $('#name')
-    let phoneno = $('#phoneno')
-    let email = $('#email')
+$(function () {
     let hostbtn = $('#hostbtn')
 
-    hostbtn.click(function(){
-        addhost(
-            name.val(),
-            phoneno.val(),
-            email.val(),
-            function (addedHost) {
-                window.alert("Added " + addedHost.name + " to Database")
-            }
-        )
+    hostbtn.click(function () {
+
+        let name = $('#name').val()
+        let phoneno = $('#phoneno').val()
+        let email = $('#email').val()
+
+        if (name.length < 1) {
+            window.alert("Name cannot be empty")
+        }
+        else if (phoneno.length < 1) {
+            window.alert("Phone Number cannot be empty")
+        }
+        else if (!(phoneno.match(/^\d{10}$/))) {
+            window.alert("Invalid Phone Number")
+        }
+        else if (email.length < 1) {
+            window.alert("Email cannot be empty")
+        }
+        else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+            window.alert("Invalid Email")
+        }
+        else {
+            addhost(
+                name,
+                phoneno,
+                email,
+                function (addedHost) {
+                    if (typeof addedHost.name !== "undefined")
+                        window.alert("Added " + addedHost.name + " to Database")
+                    else
+                        window.alert("Phone Number already registered to other host")
+                }
+            )
+        }
     })
 })
